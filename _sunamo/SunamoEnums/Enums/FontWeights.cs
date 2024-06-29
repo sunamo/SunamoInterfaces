8fc6c952a7af1435695479565686a7c6344de6dc
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 08:57:41 +0200

    moved all in _sunamo to right subfolders by original project

---
+++ _sunamo/SunamoEnums/Enums/FontWeights.cs
@@ -0,0 +1,12 @@
+namespace SunamoInterfaces;
+
+
+internal enum FontWeights : ushort
+{
+    light = 100,
+    lighter = 250,
+    normal = 400,
+    bolder = 500,
+    bold = 700,
+    extraBold = 900
+}
