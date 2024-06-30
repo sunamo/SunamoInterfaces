9c7bb7673f11d0dad3354adcdcddad3a348f2864
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-30 18:36:47 +0200

    All buildable, before removing dead code

---
+++ _public/SunamoArgs/ExternalLoginResult.cs
@@ -0,0 +1,28 @@
+namespace SunamoInterfaces;
+
+
+
+
+
+
+public class ExternalLoginResult 
+{
+    public int IdUser
+    {
+        get;
+        set;
+    }
+    public string Sc
+    {
+        get;
+        set;
+    }
+    
+    
+    
+    public string P1
+    {
+        get;
+        set;
+    }
+}
---
+++ _public/SunamoArgs/ValidateData.cs
@@ -0,0 +1,26 @@
+namespace SunamoInterfaces;
+
+
+
+
+
+public class ValidateData
+{
+    public static readonly ValidateData Default = new ValidateData();
+    public bool allowEmpty = false;
+    
+    
+    
+    public List<string> excludedStrings = new List<string>();
+    public string messageToReallyShow;
+    public string messageWhenValidateMethodFails = null;
+    public bool trim = true;
+    public Func<string, bool> validateMethod;
+    
+    
+    public int ValidateNotInline()
+    {
+        int i = 0;
+        return i;
+    }
+}
